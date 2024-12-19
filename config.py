960094e9b38ce2235d72cd7ec4cb40f51ee87a60
goat587 <goatgoat0987@gmail.com>
2024-12-19 18:26:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "6cc3cfdeae65b6ae7f47fb828edf38f4")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7070667789:AAEuSnlLysvE6iK2a8h7jHHmoQclKGjRAdM") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot-premium") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "Stubborn")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://bxbxj1280:kP47C0uyPOXRHH7w@cluster0.whdju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "bxbxj1280")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7413628447').split()]
 
 
 class temp(object): 
