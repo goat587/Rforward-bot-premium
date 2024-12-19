23c06d066ceb3280af1312ccdbce37782dcc4401
goat587 <goatgoat0987@gmail.com>
2024-12-19 18:24:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "28498858")
+    API_HASH = os.environ.get("API_HASH", "6cc3cfdeae65b6ae7f47fb828edf38f4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7070667789:AAEuSnlLysvE6iK2a8h7jHHmoQclKGjRAdM") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot-premium") 
     DB_URL = os.environ.get("DB_URL", "")
     DB_NAME = os.environ.get("DB_NAME", "Stubborn")
